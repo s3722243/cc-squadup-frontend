6cff827a80444b9e65808d3cf07effbe4efb198f
Lachlan Furlong <s3722243@student.rmit.edu.au>
2021-01-20 23:56:19 +1100

    Nicer login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,23 +1,26 @@
 import {useState} from "react";
 import {useForm} from "react-hook-form";
 import {Auth} from "@aws-amplify/auth"
-import { useHistory } from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
 
 
 export default function Login() {
     const history = useHistory();
     const [isLoading, setLoading] = useState(false);
+    const [failedMessage, setFailedMessage] = useState(null);
     const {register, handleSubmit, errors} = useForm();
 
     async function onSubmit(input) {
         setLoading(true);
         try {
             await Auth.signIn(input.username, input.password);
-            console.log("Logged in!")
-            history.push("/")
+            console.log("Logged in!");
+            history.push("/");
 
         } catch (e) {
-            console.log("An error occurred logging in:" + e)
+            console.log("An error occurred logging in:" + JSON.stringify(e));
+            setFailedMessage(e.message);
+
         }
         setLoading(false);
     }
@@ -31,23 +34,33 @@ export default function Login() {
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="field">
                                 <label className="label">SquadUp username</label>
-                                <input className="input" type="text" name="username" ref={register({required: "Your username is required!"})}/>
-                                <label className="label">Password</label>
-                                <input className="input" type="password" name="password"
-                                       ref={register({required: "Your password is required!"})}/>
-                                <div className="field is-grouped">
-                                    <div className="control">
-                                        <button
-                                            className={`button is-link ${isLoading ? "is-loading" : ""}`}
-                                            disabled={isLoading}
-                                        >
-                                            <span>Login</span>
-                                        </button>
-                                    </div>
+                                <div className="control">
+                                    <input className={`input ${errors.username ? "is-danger" : ""}`} type="text"
+                                           name="username" ref={register({required: "Your username is required!"})}/>
+                                    {errors.username &&
+                                    <div className="help is-danger">{errors.username.message}</div>}
                                 </div>
-                                {errors.username && <div className="notification is-danger">{errors.username.message}</div>}
-                                {errors.password && <div className="notification is-danger">{errors.password.message}</div>}
                             </div>
+                            <div className="field">
+                                <label className="label">Password</label>
+                                <div className="control">
+                                    <input className={`input ${errors.password ? "is-danger" : ""}`} type="password"
+                                           name="password" ref={register({required: "Your password is required!"})}/>
+                                    {errors.password &&
+                                    <div className="help is-danger">{errors.password.message}</div>}
+                                </div>
+                            </div>
+                            <div className="field">
+                                <div className="control">
+                                    <button
+                                        className={`button is-primary ${isLoading ? "is-loading" : ""}`}
+                                        disabled={isLoading}
+                                    >
+                                        <span>Login</span>
+                                    </button>
+                                </div>
+                            </div>
+                            {failedMessage && <div className="notification is-light is-danger">{failedMessage}</div>}
                         </form>
                     </div>
                 </div>
